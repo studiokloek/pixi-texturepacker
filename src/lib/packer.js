46f26e4dc935bfee08fffad905a8d82f9692fc8d
Martijn Swart <martijn@studiokloek.nl>
2021-10-26 12:02:04 +0200

    [texture] Added maxSize option, defaults to 2048

--- src/lib/packer.js
+++ src/lib/packer.js
@@ -49,6 +49,7 @@ export async function pack(directory, settings) {
       'replace': `${directoryName}=${itemPath}`,
       'extrude': get(itemOptions, 'extrude', settings.extrude) ? '1' : '0',
       'texture-format': textureFormat,
+      'max-size': get(itemOptions, 'maxSize', settings.maxSize),
     }
 
   const spinner = ora(`Packing ${itemPath}`).start();
